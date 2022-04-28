58111279abaa274a5cfaf9c4bc8c195b4838a152
Thiago Lazaro Paraguai <thiago.paraguai@agropeu.com.br>
2022-04-28 16:16:54 -0300

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,14 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import {AdMobFree} from '@ionic-native/admob-free/ngx';
+import { AdmobService } from './services/admob.service';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, AdMobFree, AdmobService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AdmobService } from '../services/admob.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit{
 
-  constructor() {}
+  constructor(private admob: AdmobService) {}
+
+  ngOnInit() {
+    this.admob.BannerAd();
+  }
+
+  showInterstitial(){
+    this.admob.InterstitialAd();
+  }
+  
+  showRewardVideo(){
+    this.admob.RewardVideoAd();
+  }
 
 }
